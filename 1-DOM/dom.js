4809132c00d8250cc35aeabba5a6889a1772b238
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2022-02-28 14:57:55 +0100

    1/2 feladat

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,12 @@
 function manipulateDom() {
     // 
+    let quotes = document.querySelectorAll(".quote");
+    quotes.forEach(q => {
+        q.style.borderColor = 'blue';
+        q.style.backgroundColor= 'lightblue';
+        q.style.fontStyle = 'italic';
+    });
+
 };
 
 export { manipulateDom };
